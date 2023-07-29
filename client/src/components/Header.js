7e64171e3fd7921fecd6d060bd56d8b41f997742
Ramu Narasinga <ramu@binlo.com.hk>
2023-07-29 11:33:16 +0100

    added more changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,10 +4,14 @@ import ViewsIcon from './assets/Views.svg';
 
 export const Header = () => {
   return (
-    <nav class="navbar navbar-expand-lg custom-navbar-bg">
+    <nav class="navbar navbar-expand-lg navbar-bg bg-dark">
       <a class="navbar-brand d-flex align-items-center" href="#">
        <img src={ViewsIcon} height='35' /> 
        <span class='views'>Views</span>
+      </a>
+      
+      <a href="https://projects.ramunarasinga.com">
+        <img src="https://visitors-count.onrender.com/api/count/incr/badge.svg?url=http://localhost:3000"/>
       </a>
     </nav>
   )
